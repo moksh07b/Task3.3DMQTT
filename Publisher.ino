61a9ff07769d81208a531c1767e288621286d649
Moksh Bansal <68209883+moksh07b@users.noreply.github.com>
2024-11-12 11:34:16 +0530

    Update Publisher.ino

--- Publisher.ino
+++ Publisher.ino
@@ -1,66 +1,66 @@
-
 #include <WiFiNINA.h>
 #include <ArduinoMqttClient.h>
 
+// WiFi credentials
 char ssid[] = "Moksh";
 char pass[] = "1234567890";
 
+// Pin configurations
 const int trigPin = 9;
 const int echoPin = 10;
-
 const int Light = 6;
 
+// Variables for distance measurement
 float duration, distance;
-float speedofSnd = 0.0343;
+float speedofSnd = 0.0343;  // Speed of sound in cm/us
 
 WiFiClient wificlient;
-
 String lstMsg = "";
 
+// MQTT client setup
 MqttClient mqttClient(wificlient);
-
 const char broker[] = "broker.emqx.io";
 int port = 1883;
+const char topic[] = "SIT210/waves";    // Topic to publish to
 
-const char topic[] = "SIT210/waves";    //Topic to publish too..
-
+// Timer variables
 const long interval = 1000;
 long prevMillis = 0;
-
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);  // Set trigPin as output
+  pinMode(echoPin, INPUT);   // Set echoPin as input
 
-  connectToWiFi();  // Connect to WiFi
-
-  connectToMQTTBroker();  //Connect to MQTT Broker
+  connectToWiFi();           // Connect to WiFi
+  connectToMQTTBroker();     // Connect to MQTT Broker
 }
 
 void loop() {
+  // Reconnect to MQTT Broker if connection is lost
   if(!mqttClient.connected()){
-    connectToMQTTBroker();  // Reconnect connection if broken
+    connectToMQTTBroker();
   }
-  mqttClient.poll();  // Handle any incoming messages.
+  mqttClient.poll();  // Handle any incoming messages
 
   long crntMillis = millis();
 
+  // Send measurement data at intervals
   if(crntMillis - prevMillis >= interval){
     prevMillis = crntMillis;
 
-    distance = readMeasurement(); // Reading Measurement
+    distance = readMeasurement();  // Get distance measurement
 
     Serial.print("Distance : ");
     Serial.println(distance);
 
-    SendMessageBasedOnDistance();
+    SendMessageBasedOnDistance();  // Send message based on distance
   }
-
 }
 
-void connectToWiFi(){   // Connect to WiFi
+// Function to connect to WiFi
+void connectToWiFi(){   
   Serial.print("Attempting to connect to ");
   Serial.println(ssid);
 
@@ -70,10 +70,10 @@ void connectToWiFi(){   // Connect to WiFi
   }
 
   Serial.println("You're Connected");
-
 }
 
-void connectToMQTTBroker(){   // Connect To MQTT Broker
+// Function to connect to the MQTT Broker
+void connectToMQTTBroker(){   
   Serial.print("Attempting to connect to ");
   Serial.println(broker);
 
@@ -84,29 +84,31 @@ void connectToMQTTBroker(){   // Connect To MQTT Broker
   Serial.println("You're Connected");
 }
 
-float readMeasurement(){    // Measuring the distance
+// Function to measure distance using ultrasonic sensor
+float readMeasurement(){    
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH, 25000);
+  duration = pulseIn(echoPin, HIGH, 25000); // Read pulse duration
 
-  return (duration * speedofSnd) / 2; // Returning the distance
+  return (duration * speedofSnd) / 2; // Calculate and return distance
 }
 
+// Function to send messages based on distance
 void SendMessageBasedOnDistance(){
-  if(distance > 0 && distance < 15){ // If distance is less than 15cm then it will send it to the Client
+  if(distance > 0 && distance < 15){ // If distance is less than 15cm, send a message
     mqttClient.beginMessage(topic);
     mqttClient.print("Hi, I'm Moksh");
     mqttClient.endMessage();
     lstMsg = "Wave";
     Serial.println("Message Sent!");
-    delay(5000);
+    delay(5000); // Wait 5 seconds before next measurement
   }
 
-  // if(distance > 15 && distance < 50){  // Additional code for pat if greater than 15cm
+  // if(distance > 15 && distance < 50){  // If distance is between 15cm and 50cm, send "Pat"
   //   mqttClient.beginMessage(topic);
   //   mqttClient.print("Pat");
   //   mqttClient.endMessage();
@@ -114,8 +116,3 @@ void SendMessageBasedOnDistance(){
   //   delay(5000);
   // }
 }
-
-
-
-
-
